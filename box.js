1b26c7732094a991d60c7cc02441200ef1375c89
Skymaster640 <skylar.TheHouseRobinson@gmail.com>
2020-10-06 15:50:34 -0500

    Add files via upload

---
+++ box.js
@@ -0,0 +1,40 @@
+class Box{
+    constructor(x, y, width, height) {
+        var options = {
+            isStatic:false,
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        this.Visibility = 255;
+        World.add(world, this.body);
+      }
+      display(){
+
+        if(this.body.speed < 3){
+        var pos = this.body.position;
+        var angle = this.body.angle;
+
+        push();
+
+        rotate(angle);
+        rectMode(CENTER);
+
+
+        fill (random (50,255),random(50,255),random(50,255));
+        strokeWeight(4);
+        stroke("black");
+
+        rect(pos.x, pos.y, this.width, this.height);
+        pop();
+      }
+      else{
+        push();
+        World.remove(world, this.body);
+        this.Visibility = this.Visibility - 5;
+        tint(255,this.Visibility);
+        pop();
+      }
+
+      }
+}
